25c120d406c8007c6be43595b1ac6fa4c1252651
Murray Lowery-Simpson <murraylowerysimpson@gmail.com>
2022-01-17 22:41:30 -0400

    Updated calibration code for IMUTest

--- IMUTest.cpp
+++ IMUTest.cpp
@@ -1,3 +1,5 @@
+#define IMUTEST_BUILD 1
+
 #include "../RemoteControlTest/IMU.h"
 #include <pthread.h>
 #include <iostream>
@@ -5,6 +7,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <wiringPi.h>
 #include <memory>
 
 
@@ -28,6 +31,93 @@ bool isMagCalFlagPresent(int argc, char * argv[]) {
 	return false;
 }
 
+/**
+ * @brief return true if a factory XY magnetometer calibration flag (-fmxy) was specified in the program arguments
+ *
+ * @param argc the number of program arguments
+ * @param argv an array of character pointers that corresponds to the program arguments
+ * @return true if a factory XY magnetometer calibration flag (-fmxy) is present in the array of program arguments
+ * @return false if no factory XY magnetometer calibration flag is present in the array of program arguments.
+ */
+bool isFactoryMagCalXYFlagPresent(int argc, char* argv[]) {
+    for (int i = 0; i < argc; i++) {
+        if (strlen(argv[i]) < 5) continue;
+        if (strncmp(argv[i], "-fmxy", 5) == 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
+/**
+ * @brief return true if a factory XZ magnetometer calibration flag (-fmxz) was specified in the program arguments
+ *
+ * @param argc the number of program arguments
+ * @param argv an array of character pointers that corresponds to the program arguments
+ * @return true if a factory XZ magnetometer calibration flag (-fmxz) is present in the array of program arguments
+ * @return false if no factory XZ magnetometer calibration flag is present in the array of program arguments.
+ */
+bool isFactoryMagCalXZFlagPresent(int argc, char* argv[]) {
+    for (int i = 0; i < argc; i++) {
+        if (strlen(argv[i]) < 5) continue;
+        if (strncmp(argv[i], "-fmxz", 5) == 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
+/**
+ * @brief return true if a factory temperature calibration flag (-ftempcal) was specified in the program arguments
+ *
+ * @param argc the number of program arguments
+ * @param argv an array of character pointers that corresponds to the program arguments
+ * @return true if a factory temperature calibration flag (-ftempcal) is present in the array of program arguments
+ * @return false if no factory temperature calibration flag is present in the array of program arguments.
+ */
+bool isTempCalFlagPresent(int argc, char* argv[]) {
+    for (int i = 0; i < argc; i++) {
+        if (strlen(argv[i]) < 9) continue;
+        if (strncmp(argv[i], "-ftempcal", 9) == 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
+/**
+ * @brief return true if a -h or -H help flag was specified in the program arguments
+ *
+ * @param argc the number of program arguments
+ * @param argv an array of character pointers that corresponds to the program arguments
+ * @return true if a help flag (-h or -H) is present in the array of program arguments
+ * @return false if no help flag is present in the array of program arguments.
+ */
+bool isHelpFlagPresent(int argc, char* argv[]) {
+    for (int i = 0; i < argc; i++) {
+        if (strlen(argv[i]) < 2) continue;
+        if (strncmp(argv[i], "-h", 2) == 0) {
+            return true;
+        }
+        else if (strncmp(argv[i], "-H", 2) == 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void ShowIMUTestUsage() {
+    printf("IMUTest\n");
+    printf("Usage: IMUTest [-h] [-magcal] [-fmxy] [-fmxz] [-ftempcal]\n");
+    printf("If no arguements are specified, the program collects and prints out data from the IMU for about 5 seconds.\n");
+    printf("Optional flags:\n");
+    printf("-h: prints out this help message.\n");
+    printf("-magcal: does a calibration of the X and Y magnetometers by assuming that the device is rotated slowly in a complete circle.\n");
+    printf("-fmxy: does a factory calibration of the X and Y magnetometers (similar to that done with the -magcal flag) except that it requires the user to toggle calibration data sampling on and off with the press of a button.\n");
+    printf("-fmxz: does a factory calibration of the X and Z magnetometers (requires IMU device to be aligned on edge with Y-axis pointed up or down) and requires the user to toggle calibration data sampling on and off with the press of a button.\n");
+    printf("-ftempcal: does a factory temperature calibration.\n");
+}
+
 
 /**
  * @brief the main function for the IMUTest program
@@ -43,14 +133,43 @@ int main(int argc, char * argv[])
   pthread_mutex_t i2cMutex = PTHREAD_MUTEX_INITIALIZER;;//mutex for controlling access to i2c bus
   IMU imu(&i2cMutex);
   if (imu.m_bInitError) {
-	  printf("An error occurred trying to initialized the IMU.\n");
+	  printf("An error occurred trying to initialize the IMU.\n");
 	  return -1;
   }
   if (isMagCalFlagPresent(argc,argv)) {
-    if (!imu.DoMagCal()) {
+    if (!imu.DoXYMagCal()) {
       printf("Error doing magnetometer calibration.\n");
       return -2;
     }
+    return 0;
+  }
+  else if (isHelpFlagPresent(argc, argv)) {
+      ShowIMUTestUsage();
+      return 0;
+  }
+  else if (isFactoryMagCalXYFlagPresent(argc, argv)) {
+      wiringPiSetupGpio();//need to call this before doing anything with GPIO
+      if (!imu.DoXYMagCalWithToggledSampling()) {
+          printf("Error doing factory magnetometer XY calibration.\n");
+          return -4;
+      }
+      return 0;
+  }
+  else if (isFactoryMagCalXZFlagPresent(argc, argv)) {
+      wiringPiSetupGpio();//need to call this before doing anything with GPIO
+      if (!imu.DoXZMagCalWithToggledSampling()) {
+          printf("Error doing factory magnetometer XZ calibration.\n");
+          return -5;
+      }
+      return 0;
+  }
+  else if (isTempCalFlagPresent(argc, argv)) {
+      wiringPiSetupGpio();//need to call this before doing anything with GPIO
+      if (!imu.DoTempCal()) {
+          printf("Error doing factory temperature calibration.\n");
+          return -6;
+      }
+      return 0;
   }
   IMU_DATASAMPLE imu_sample;
   for (int i=0;i<NUM_SAMPLES;i++) {
